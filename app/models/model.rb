6e769921ddd0c0b5b451af8004c047205af1f079
Linaly1018 <miyamoto.linaly@gmail.com>
2018-08-01 16:28:51 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,8 +1,10 @@
 class Dog
   attr_accessor :name, :breed, :name 
+  @@dog_count = 0 
   def initialize(name, breed, name)
     @name = name
     @breed = breed
     @age = age
   end
+  @@dog_count
 end
